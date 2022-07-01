bf21108d9bbcd8ad20662b60c2ff220a9cc493a5
joshybaDev <joshybacorp@gmail.com>
2022-06-30 21:13:01 -0500

    Se creó la migración para guardar la dirección en la orden.

---
+++ database/migrations/2022_07_01_020933_create_order_addresses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders');            
+            $table->string('street',200);
+            $table->string('city',100);
+            $table->string('state',100);
+            $table->string('zipcode',5);            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_addresses');
+    }
+};
